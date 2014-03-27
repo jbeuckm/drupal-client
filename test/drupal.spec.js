ed8b3c11487191c0f7e447dd99a0b6820772d93c
joe beuckman <joebeuckman@gmail.com>
2014-03-26 19:48:32 -0500

    quit test if no config

--- test/drupal.spec.js
+++ test/drupal.spec.js
@@ -1,291 +1,292 @@
 describe("Drupal Tests", function () {
 
-    var drupal = require('../lib/drupal.js');
+  var drupal = require('../lib/drupal.js');
 
-    try {
-        var cfg = require("./config.json");
-        drupal.setRestPath(cfg.SITE_ROOT, cfg.SERVICES_ENDPOINT);
-        console.log("found drupal config: "+JSON.stringify(cfg));
-    }
-    catch (e) {
-        console.log("unable to load drupal config file");
-    }
-
-
-    describe("can create account & login", function () {
-
-        var username = 'drupalspec' + createRandomString(8);
-        var user = {
-            name:username,
-            pass:createRandomString(8),
-            mail:username + '@drupalspec.com',
-            status:1
-        };
-        var uid = 0;
-
-        logoutIfNecessary();
+  try {
+    var cfg = require("./config.json");
+    drupal.setRestPath(cfg.SITE_ROOT, cfg.SERVICES_ENDPOINT);
+    console.log("found drupal config: " + JSON.stringify(cfg));
+  }
+  catch (e) {
+    console.log("unable to load drupal config file");
+    return;
+  }
 
 
-        it("calls system/connect", function () {
+  describe("can create account & login", function () {
 
-            var connected = false;
-            var done = false;
+    var username = 'drupalspec' + createRandomString(8);
+    var user = {
+      name: username,
+      pass: createRandomString(8),
+      mail: username + '@drupalspec.com',
+      status: 1
+    };
+    var uid = 0;
 
-            runs(function () {
-
-                drupal.systemConnect(
-                    function (responseData) {
-                        uid = responseData.user.uid;
-                        console.log("system/connect reported uid " + uid);
-                        connected = true;
-                        done = true;
-                    },
-                    function (err) {
-                        connected = false;
-                        done = true;
-                    }
-                );
-            });
-
-            waitsFor(function () {
-                return done;
-            }, 'timeout connecting', 2500);
-
-            runs(function () {
-                expect(connected).toEqual(true);
-            });
-        });
-
-        logoutIfNecessary();
-
-        it("registers an account", function () {
-
-            var done = false;
-            var error = '';
-            var response = '';
-
-            runs(function () {
-
-                drupal.createAccount(user,
-                    //success
-                    function (res) {
-                        response = res;
-                        done = true;
-                    },
-                    //failure
-                    function (e) {
-                        error = e;
-                        done = true;
-                    }
-                );
-            });
-
-            waitsFor(function () {
-                return done;
-            }, 'timeout creating account', 2500);
-
-            runs(function () {
-                expect(error).toEqual('');
-            });
-
-        });
+    logoutIfNecessary();
 
 
-        it("can log in", function () {
+    it("calls system/connect", function () {
 
-            var loggedin = false;
-            var done = false;
+      var connected = false;
+      var done = false;
 
-            // login as the previously created test user
-            runs(function () {
-                drupal.login(user.name, user.pass,
-                    function (data) {
-                        console.log('spec login succeeded with uid ' + data.uid);
-                        uid = data.uid;
-                        loggedin = true;
-                        done = true;
-                    },
-                    function (err) {
-                        console.log(err);
-                        loggedin = false;
-                        done = true;
-                    }
-                );
-            });
+      runs(function () {
 
-            waitsFor(function () {
-                return done;
-            }, 'timeout logging in', 2500);
-
-        });
-
-
-        it("can load user entity", function () {
-
-            var done = false;
-            var success = false;
-
-            runs(function () {
-                drupal.getResource('user/' + uid, null,
-                    function (data) {
-                        done = true;
-                        success = true;
-                    },
-                    function (err) {
-                        console.log(err);
-                        done = true;
-                        success = false;
-                    }
-                );
-            });
-
-            waitsFor(function () {
-                return done;
-            }, 'timeout loading my user', 2500);
-        });
-
-
-      it("can create a node", function () {
-
-        var success = false;
-        var done = false;
-
-        var node = {
-          type:"article",
-          title:drupal.basicField("test node title"),
-          body:drupal.basicField("test node body"),
-        };
-
-        runs(function () {
-          drupal.postResource('node', node,
-            function () {
-              console.log('POST succeeded');
-              success = true;
-              done = true;
-            },
-            function (err) {
-              console.log(err);
-              success = false;
-              done = true;
-            }
-          );
-        });
-
-        waitsFor(function () {
-          return done;
-        }, 'timeout posting a node', 2500);
+        drupal.systemConnect(
+          function (responseData) {
+            uid = responseData.user.uid;
+            console.log("system/connect reported uid " + uid);
+            connected = true;
+            done = true;
+          },
+          function (err) {
+            connected = false;
+            done = true;
+          }
+        );
       });
 
+      waitsFor(function () {
+        return done;
+      }, 'timeout connecting', 2500);
 
-      it("can upload a file", function () {
+      runs(function () {
+        expect(connected).toEqual(true);
+      });
+    });
 
-        var success = false;
-        var done = false;
+    logoutIfNecessary();
 
-        runs(function () {
-          drupal.uploadFile(base64data, filename, filesize,
-            function () {
-              console.log('POST succeeded');
-              success = true;
-              done = true;
-            },
-            function (err) {
-              console.log(err);
-              success = false;
-              done = true;
-            }
-          );
-        });
+    it("registers an account", function () {
 
-        waitsFor(function () {
-          return done;
-        }, 'timeout posting a node', 2500);
+      var done = false;
+      var error = '';
+      var response = '';
 
+      runs(function () {
+
+        drupal.createAccount(user,
+          //success
+          function (res) {
+            response = res;
+            done = true;
+          },
+          //failure
+          function (e) {
+            error = e;
+            done = true;
+          }
+        );
       });
 
+      waitsFor(function () {
+        return done;
+      }, 'timeout creating account', 2500);
 
-      it("can log out", function () {
-
-            var loggedout = false;
-            var done = false;
-
-            runs(function () {
-                if (uid != 0) {
-                    drupal.logout(
-                        function () {
-                            loggedout = true;
-                            done = true;
-                        },
-                        function () {
-                            loggedout = false;
-                            done = true;
-                        }
-                    );
-                }
-                else {
-                    loggedout = true;
-                }
-            });
-
-            waitsFor(function () {
-                return done;
-            }, "timeout logging out", 2500);
-
-        });
+      runs(function () {
+        expect(error).toEqual('');
+      });
 
     });
 
 
-    describe("deals with Drupal data types", function () {
+    it("can log in", function () {
 
-        it("serializes filter parameters for drupal", function () {
-            var params = {
-                'arg[]':[1, 2, 3]
-            };
-            var request = drupal.serializeDrupalViewsFilter(params);
+      var loggedin = false;
+      var done = false;
 
-            expect(decodeURIComponent(request)).toEqual('arg[]=1&arg[]=2&arg[]=3');
-        });
+      // login as the previously created test user
+      runs(function () {
+        drupal.login(user.name, user.pass,
+          function (data) {
+            console.log('spec login succeeded with uid ' + data.uid);
+            uid = data.uid;
+            loggedin = true;
+            done = true;
+          },
+          function (err) {
+            console.log(err);
+            loggedin = false;
+            done = true;
+          }
+        );
+      });
+
+      waitsFor(function () {
+        return done;
+      }, 'timeout logging in', 2500);
 
     });
 
 
-    function logoutIfNecessary() {
+    it("can load user entity", function () {
 
-        var token = drupal.Settings.getString("X-CSRF-Token");
-        var cookie = drupal.Settings.getString("Drupal-Cookie");
-        if (token && cookie) {
+      var done = false;
+      var success = false;
 
-            it("logs out if necessary", function () {
+      runs(function () {
+        drupal.getResource('user/' + uid, null,
+          function (data) {
+            done = true;
+            success = true;
+          },
+          function (err) {
+            console.log(err);
+            done = true;
+            success = false;
+          }
+        );
+      });
 
-                var loggedout = false;
-                var done = false;
+      waitsFor(function () {
+        return done;
+      }, 'timeout loading my user', 2500);
+    });
 
-                runs(function () {
 
-                    drupal.logout(
-                        function () {
-                            drupal.Settings.setString("X-CSRF-Token", null);
-                            drupal.Settings.setString("Drupal-Cookie", null);
-                            loggedout = true;
-                            done = true;
-                        },
-                        function () {
-                            loggedout = false;
-                            done = true;
-                        }
-                    );
-                });
+    it("can create a node", function () {
 
-                waitsFor(function () {
-                    return done;
-                }, "timeout logging out", 2500);
+      var success = false;
+      var done = false;
 
-            });
+      var node = {
+        type: "article",
+        title: drupal.basicField("test node title"),
+        body: drupal.basicField("test node body"),
+      };
 
+      runs(function () {
+        drupal.postResource('node', node,
+          function () {
+            console.log('POST succeeded');
+            success = true;
+            done = true;
+          },
+          function (err) {
+            console.log(err);
+            success = false;
+            done = true;
+          }
+        );
+      });
+
+      waitsFor(function () {
+        return done;
+      }, 'timeout posting a node', 2500);
+    });
+
+
+    it("can upload a file", function () {
+
+      var success = false;
+      var done = false;
+
+      runs(function () {
+        drupal.uploadFile(base64data, filename, filesize,
+          function () {
+            console.log('POST succeeded');
+            success = true;
+            done = true;
+          },
+          function (err) {
+            console.log(err);
+            success = false;
+            done = true;
+          }
+        );
+      });
+
+      waitsFor(function () {
+        return done;
+      }, 'timeout posting a node', 2500);
+
+    });
+
+
+    it("can log out", function () {
+
+      var loggedout = false;
+      var done = false;
+
+      runs(function () {
+        if (uid != 0) {
+          drupal.logout(
+            function () {
+              loggedout = true;
+              done = true;
+            },
+            function () {
+              loggedout = false;
+              done = true;
+            }
+          );
         }
+        else {
+          loggedout = true;
+        }
+      });
+
+      waitsFor(function () {
+        return done;
+      }, "timeout logging out", 2500);
+
+    });
+
+  });
+
+
+  describe("deals with Drupal data types", function () {
+
+    it("serializes filter parameters for drupal", function () {
+      var params = {
+        'arg[]': [1, 2, 3]
+      };
+      var request = drupal.serializeDrupalViewsFilter(params);
+
+      expect(decodeURIComponent(request)).toEqual('arg[]=1&arg[]=2&arg[]=3');
+    });
+
+  });
+
+
+  function logoutIfNecessary() {
+
+    var token = drupal.Settings.getString("X-CSRF-Token");
+    var cookie = drupal.Settings.getString("Drupal-Cookie");
+    if (token && cookie) {
+
+      it("logs out if necessary", function () {
+
+        var loggedout = false;
+        var done = false;
+
+        runs(function () {
+
+          drupal.logout(
+            function () {
+              drupal.Settings.setString("X-CSRF-Token", null);
+              drupal.Settings.setString("Drupal-Cookie", null);
+              loggedout = true;
+              done = true;
+            },
+            function () {
+              loggedout = false;
+              done = true;
+            }
+          );
+        });
+
+        waitsFor(function () {
+          return done;
+        }, "timeout logging out", 2500);
+
+      });
 
     }
+
+  }
 
 
 });
@@ -293,14 +294,14 @@ describe("Drupal Tests", function () {
 
 function createRandomString(max) {
 
-    if (max == null) max = 20;
-    var text = "";
-    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  if (max == null) max = 20;
+  var text = "";
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-    for (var i = 0; i < max; i++) {
-        text += possible.charAt(Math.floor(Math.random() * possible.length));
-    }
+  for (var i = 0; i < max; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
 
-    return text;
+  return text;
 }
 
